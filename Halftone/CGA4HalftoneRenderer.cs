d8bf7591a145058b550463276a05dcf03c9f6b0e
Parking Lot Studio <76890242+ParkingLotGames@users.noreply.github.com>
2023-03-10 10:11:44 -0600

    Update CGA4HalftoneRenderer.cs
    Remove CG specific parts as it now uses HLSL

--- Halftone/CGA4HalftoneRenderer.cs
+++ Halftone/CGA4HalftoneRenderer.cs
@@ -4,14 +4,7 @@ using UnityEngine.Rendering.PostProcessing;
 public sealed class CGA4HalftoneRenderer : PostProcessEffectRenderer<CGA4Halftone>
 { 
     readonly int contributionID = Shader.PropertyToID("_Contribution");
-    Material cga4Material;
-
-    public override void Init()
-    {
-        Shader cga4Shader = Shader.Find("Hidden/CGAHalftone");
-        cga4Material = new Material(cga4Shader);
-    }
-
+    
     public override void Render(PostProcessRenderContext context)
     {
         var sheet = context.propertySheets.Get(Shader.Find("Hidden/CGAHalftone"));
